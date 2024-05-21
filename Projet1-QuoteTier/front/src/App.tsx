1e889eed60ed1f3cde4b224b05344c1fc1ac9fc6
Romuald_Hanriot <56790180+Phyrum78@users.noreply.github.com>
2024-05-21 10:27:55 +0200

    add routing

--- Projet1-QuoteTier/front/src/App.tsx
+++ Projet1-QuoteTier/front/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import Home from './pages/home';
+import Quote from './pages/quote';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Projet1-QuoteTier/front/src/pages/quote.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Quote = () => {
+  return <h1>This is the Quote page</h1>;
+};
+
+export default Quote;
