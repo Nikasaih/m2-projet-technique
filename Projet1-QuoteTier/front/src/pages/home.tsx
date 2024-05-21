0a9aba96e437643f3e187e3845fadf2910b81dfe
Romuald_Hanriot <56790180+Phyrum78@users.noreply.github.com>
2024-05-21 11:53:27 +0200

    get quotes

--- Projet1-QuoteTier/front/src/pages/home.tsx
+++ Projet1-QuoteTier/front/src/pages/home.tsx
@@ -1,44 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import './home.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 
 type Quote = {
   id: number;
-  text: string;
+  quote: string;
   author: string;
 };
 
 const Home = () => {
+  const navigate = useNavigate();
   const [quotes, setQuotes] = useState<Quote[]>([]);
 
+  const handleCreateQuote = () => {
+    navigate("/add-quote");
+  };
+
   useEffect(() => {
-    // Simuler la récupération des données
-    const fetchedQuotes: Quote[] = [
-      { id: 1, text: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" },
-      { id: 2, text: "Do not watch the clock. Do what it does. Keep going.", author: "Sam Levenson" },
-      { id: 3, text: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer" },
-      { id: 4, text: "Life is 10% what happens to us and 90% how we react to it.", author: "Charles R. Swindoll" },
-    ];
-
-    setQuotes(fetchedQuotes);
+    fetch("http://localhost:5000/quotes")
+      .then((response) => response.json())
+      .then((data) => {
+        const quotes = data.map((quote: string) => JSON.parse(quote));
+        setQuotes(quotes);
+      })
+      .catch((error) => console.error("Error fetching quotes:", error));
   }, []);
-
-//   useEffect(() => {
-//     fetch('http://localhost:3001/quotes')
-//       .then(response => response.json())
-//       .then(data => setQuotes(data))
-//       .catch(error => console.error('Error fetching quotes:', error));
-//   }, []);
 
   return (
     <div className="container">
       <input type="text" placeholder="Search..." />
-      <button onClick={() => alert('Create Quote button clicked!')}>
-        Create Quote
-      </button>
+      <button onClick={handleCreateQuote}>Create Quote</button>
       <div className="quotes-list">
         {quotes.map((quote) => (
           <div key={quote.id} className="quote-item">
-            <p className="quote-text">"{quote.text}"</p>
+            <p className="quote-text">"{quote.quote}"</p>
             <p className="quote-author">- {quote.author}</p>
           </div>
         ))}
