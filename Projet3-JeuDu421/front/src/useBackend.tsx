0978440b82ae7058aadabcc1918e40fdbc5c3ce9
Nikasaih <gchaducnguyen@gmail.com>
2024-05-23 09:16:11 +0200

    ajout d'axios pour request le back

--- Projet3-JeuDu421/front/src/useBackend.tsx
+++ Projet3-JeuDu421/front/src/useBackend.tsx
@@ -1,21 +1,59 @@
 import { useState } from "react";
 import { IGame } from "./interface";
+import axios from "axios";
+const host = "http://localhost:5000";
 
 export const useBackend = () => {
   const [myDice, setMyDices] = useState<number[]>([]);
   const [gameList, setGameList] = useState<IGame[]>([]);
-  const createGame = () => {};
-  const startGame = () => {};
-  const joinGame = () => {};
-  const cancelGame = () => {};
-  const listGames = () => {};
-  const quitGame = () => {};
+  const [currGame, setCurrGame] = useState<IGame | undefined>();
+  const createGame = async () => {
+    const c = await axios.post(host + "/create-game");
+    setCurrGame(c.data);
+  };
+  const startGame = () => {
+    if (currGame) {
+      axios.post(host + "/start-game", { gameId: currGame.gameId });
+    }
+  };
+  const joinGame = async (gameId: string) => {
+    const c = await axios.post(host + "/join-game", { gameId: gameId });
+
+    setCurrGame(c.data);
+  };
+  const cancelGame = async (gameId: string) => {
+    const c = await axios.post(host + "/cancel-game", { gameId: gameId });
+
+    if (c.status === 200) {
+      setCurrGame(undefined);
+    }
+  };
+  const listGames = async () => {
+    const games = await axios.get(host + "/list-games");
+    setGameList(games.data);
+  };
+  const quitGame = async (gameId: string) => {
+    const c = await axios.post(host + "/quit-game", { gameId: gameId });
+
+    if (c.status === 200) {
+      setCurrGame(undefined);
+    }
+  };
   const throwDice = () => {
+    if (!currGame) {
+      return;
+    }
     const value1 = 4;
     const value2 = 2;
     const value3 = 1;
 
-    setMyDices([value1, value2, value3]);
+    const throwResult = [value1, value2, value3];
+    setMyDices(throwResult);
+
+    axios.post(host + "/check-dice", {
+      diceResults: throwResult,
+      gameId: currGame.gameId,
+    });
   };
 
   return {
@@ -26,7 +64,7 @@ export const useBackend = () => {
     listGames,
     quitGame,
     throwDice,
-    games: gameList,
+    gameList,
     myDice,
   };
 };
