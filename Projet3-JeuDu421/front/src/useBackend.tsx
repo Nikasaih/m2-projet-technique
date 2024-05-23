58b9763782ccac4f231a174a44ba69f4a4190b67
Nikasaih <gchaducnguyen@gmail.com>
2024-05-23 09:22:52 +0200

    ajout des websocket coté front

--- Projet3-JeuDu421/front/src/useBackend.tsx
+++ Projet3-JeuDu421/front/src/useBackend.tsx
@@ -1,39 +1,40 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IGame } from "./interface";
 import axios from "axios";
-const host = "http://localhost:5000";
-
+const port = 5000;
+const httpHost = "http://localhost:" + port;
+const wsHost = "ws://localhost:" + port;
 export const useBackend = () => {
   const [myDice, setMyDices] = useState<number[]>([]);
   const [gameList, setGameList] = useState<IGame[]>([]);
   const [currGame, setCurrGame] = useState<IGame | undefined>();
   const createGame = async () => {
-    const c = await axios.post(host + "/create-game");
+    const c = await axios.post(httpHost + "/create-game");
     setCurrGame(c.data);
   };
   const startGame = () => {
     if (currGame) {
-      axios.post(host + "/start-game", { gameId: currGame.gameId });
+      axios.post(httpHost + "/start-game", { gameId: currGame.gameId });
     }
   };
   const joinGame = async (gameId: string) => {
-    const c = await axios.post(host + "/join-game", { gameId: gameId });
+    const c = await axios.post(httpHost + "/join-game", { gameId: gameId });
 
     setCurrGame(c.data);
   };
   const cancelGame = async (gameId: string) => {
-    const c = await axios.post(host + "/cancel-game", { gameId: gameId });
+    const c = await axios.post(httpHost + "/cancel-game", { gameId: gameId });
 
     if (c.status === 200) {
       setCurrGame(undefined);
     }
   };
   const listGames = async () => {
-    const games = await axios.get(host + "/list-games");
+    const games = await axios.get(httpHost + "/list-games");
     setGameList(games.data);
   };
   const quitGame = async (gameId: string) => {
-    const c = await axios.post(host + "/quit-game", { gameId: gameId });
+    const c = await axios.post(httpHost + "/quit-game", { gameId: gameId });
 
     if (c.status === 200) {
       setCurrGame(undefined);
@@ -50,11 +51,22 @@ export const useBackend = () => {
     const throwResult = [value1, value2, value3];
     setMyDices(throwResult);
 
-    axios.post(host + "/check-dice", {
+    axios.post(httpHost + "/check-dice", {
       diceResults: throwResult,
       gameId: currGame.gameId,
     });
   };
+
+  const websocket = useCallback(() => {
+    const socket = new WebSocket(wsHost);
+    socket.onmessage = function (event) {
+      console.log("Received message:", event.data);
+    };
+  }, []);
+
+  useEffect(() => {
+    websocket();
+  }, [websocket]);
 
   return {
     joinGame,
